b62abf824cb2449a76c5ef2328b046b5a7d9e12a
andriy-lanyk <LAM1988@ukr.net>
2021-05-23 09:38:51 +0300

    refactor code

--- index.js
+++ index.js
@@ -47,20 +47,20 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction) => {
+const makeTransaction = transaction => {
   const delay = randomIntegerFromInterval(200, 500);
 
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
+      
       if (canProcess) {
-        resolve([transaction.id, delay])
+        resolve([transaction.id, delay]);
       } else {
-        reject(transaction.id)
-      }  }, delay);
-  })
-
-
+        reject(transaction.id);
+      };
+    }, delay);
+  });
 };
 
 const logSuccess = ([id, time]) => {
@@ -71,9 +71,6 @@ const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
-/*
- * Должно работать так
- */
 makeTransaction({ id: 70, amount: 150 })
   .then(logSuccess)
   .catch(logError);

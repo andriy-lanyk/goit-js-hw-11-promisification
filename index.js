b5e64ee1a989585aad9c8f14ddf26adc78bf063a
andriy-lanyk <LAM1988@ukr.net>
2021-05-23 09:32:55 +0300

    Finished

--- index.js
+++ index.js
@@ -1,35 +1,91 @@
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
+//Task #2
+console.log('Task #1');
+const delay = ms => {
+  return new Promise(() => logger1(ms))
+};
+
+const logger1 = time => console.log(`Resolved after ${time}ms`);
+
+// Вызовы функции для проверки
+delay(2000).then(logger1); // Resolved after 2000ms
+delay(1000).then(logger1); // Resolved after 1000ms
+delay(1500).then(logger1); // Resolved after 1500ms
+
+
+//Task #2
+console.log('-----------------');
+console.log('Task #2');
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
 ];
 
-const btnStart = document.querySelector('[data-action="start"]');
-const btnStop = document.querySelector('[data-action="stop"]');
-let timerID = null;
+const toggleUserState = (allUsers, userName) => {
+  const updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+
+  return new Promise(() => logger2(updatedUsers));
+};
+
+const logger2 = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger2);
+toggleUserState(users, 'Lux').then(logger2);
+
+
+// TASK #3
+console.log('-----------------');
+console.log('Task #3');
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-btnStart.addEventListener('click', changeBodyColor);
+const makeTransaction = (transaction) => {
+  const delay = randomIntegerFromInterval(200, 500);
 
-btnStop.addEventListener('click', stopChangeBodyColor);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+      if (canProcess) {
+        resolve([transaction.id, delay])
+      } else {
+        reject(transaction.id)
+      }  }, delay);
+  })
 
-function changeBodyColor() {
-    document.body.style.backgroundColor = `${changeColor(colors)}`;
-    btnStart.setAttribute("disabled", true)
-    timerID = setTimeout(changeBodyColor, 1000);
+
 };
 
-function changeColor(colors) {
-    return colors[randomIntegerFromInterval(0, colors.length - 1)];
+const logSuccess = ([id, time]) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
-function stopChangeBodyColor() {
-    clearTimeout(timerID);
-    btnStart.removeAttribute("disabled")
-}
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+/*
+ * Должно работать так
+ */
+makeTransaction({ id: 70, amount: 150 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 71, amount: 230 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 72, amount: 75 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 73, amount: 100 })
+  .then(logSuccess)
+  .catch(logError);
